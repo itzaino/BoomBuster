356fbb0e8e966b9aa159eb1fbfcbc6ab22ddd4c9
Vincent Zygar <lub@blub.com>
2023-07-10 12:41:39 +0200

    lol

--- src/Think.java
+++ src/Think.java
@@ -1,23 +1,31 @@
 import types.Field;
 import types.Level;
-
+import java.util.Random;
 import java.util.Arrays;
 
 public class Think {
-    public static void gameInitiation(String levelC){
+    public static void gameInitiation(String levelC) {
         for (Level levelL : Main.levels) {
-            if(levelC.charAt(0)==levelL.letter){
-                Main.level=levelL;
+            if (levelC.charAt(0) == levelL.letter) {
+                Main.level = levelL;
             }
         }
-        Main.board=new Field[Main.level.fieldSize][Main.level.fieldSize];
-        for(Field[] row:Main.board){
-            Arrays.setAll(row, i->new Field());
+        Main.board = new Field[Main.level.fieldSize][Main.level.fieldSize];
+        for (Field[] row : Main.board) {
+            Arrays.setAll(row, i -> new Field());
         }
         //Mine Randomisation --> see bottom, call here
         //Mine Counter --> loop here, function in Field
     }
-    public static void mineRandomiser(){
 
+    public static void mineRandomiser() {
+        Random random = new Random();
+        while (Level.minesAmount > 0) {
+            Field Field = Main.board[random.nextInt(Main.level.fieldSize)][random.nextInt(Main.level.fieldSize)];
+            if (!(Field.mine)) {
+                Field.mine = true;
+                Level.minesAmount--;
+            }
+        }
     }
 }
--- src/types/Level.java
+++ src/types/Level.java
@@ -4,7 +4,7 @@ public class Level {
     public String name;
     public char letter;
     public int fieldSize;
-    public int minesAmount;
+    public static int minesAmount;
 
     public Level(String name, char letter, int fieldSize, int minesAmount){
         this.name=name;

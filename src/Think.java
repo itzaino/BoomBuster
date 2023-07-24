2b8397176ed56466dc9966b710eefbdc4574b249
adi <adrian.fougerouse@gmail.com>
2023-07-24 12:11:32 +0200

    think edit

--- src/Think.java
+++ src/Think.java
@@ -30,31 +30,43 @@ public class Think {
         int x = 0;
         int y = 0;
         int counter = 0;
-        for (x = 0; x <= Main.level.fieldSize; x++){
-            for ( y = 0; y <= Main.level.fieldSize; y++){
-                if (Main.board[x-1][y-1].mine == true){
-                    counter = counter + 1;
+        for (x = 0; x < Main.level.fieldSize; x++){
+            for ( y = 0; y < Main.level.fieldSize; y++){
+                if (x > 0 & y > 0) {
+                    if (Main.board[x - 1][y - 1].mine == true){
+                        counter = counter + 1;
+                    }
                 }
-                if (Main.board[x-1][y].mine == true){
-                    counter = counter + 1;
+                if (x > 0){
+                    if (Main.board[x - 1][y].mine == true){
+                        counter = counter + 1;
+                    }
+
                 }
-                if (Main.board[x-1][y+1].mine == true){
-                    counter = counter + 1;
+                if (x > 0 & y < Main.level.fieldSize){
+                    if (Main.board[x - 1][y + 1].mine == true){
+                        counter = counter + 1;
+                    }
                 }
-                if (Main.board[x][y-1].mine == true){
-                    counter = counter + 1;
+                if (y > 0){
+                    if (Main.board[x][y - 1].mine == true) {
+                            counter = counter + 1;
+                    }
                 }
-                if (Main.board[x][y+1].mine == true){
-                    counter = counter + 1;
+                if (x < Main.level.fieldSize){
+                    if (Main.board[x+1][y].mine == true){
+                        counter = counter + 1;
+                    }
                 }
-                if (Main.board[x+1][y-1].mine == true){
-                    counter = counter + 1;
+                if (y < Main.level.fieldSize){
+                    if (Main.board[x][y + 1].mine == true) {
+                        counter = counter + 1;
+                    }
                 }
-                if (Main.board[x+1][y].mine == true){
-                    counter = counter + 1;
-                }
-                if (Main.board[x+1][y+1].mine == true){
-                    counter = counter + 1;
+                if (x < Main.level.fieldSize & y < Main.level.fieldSize){
+                    if (Main.board[x + 1][y + 1].mine == true) {
+                        counter = counter + 1;
+                    }
                 }
                 Main.board[x][y].number=counter;
                 counter = 0;
